79f94910d69a25e09a00b893bf4607b659231e6d
Nelson Medel <nmedelb@gmail.com>
2023-07-12 13:44:25 -0400

    Se crea el UsuarioController, con esto toma el include

---
+++ src/main/java/cl/cristiandurf/gestorliquidaciones/controller/InicioController.java
@@ -0,0 +1,25 @@
+package cl.cristiandurf.gestorliquidaciones.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * @author El Piero tiene sueño
+ * @version 0.0.1
+ * @since 28-06-2022
+ */
+@Controller
+@RequestMapping("/")
+public class InicioController {
+    Log logger = LogFactory.getLog(InicioController.class);
+    @GetMapping
+    public String inicio(){
+
+        logger.info("Estás en el home");
+        return "Home";
+    }
+}
