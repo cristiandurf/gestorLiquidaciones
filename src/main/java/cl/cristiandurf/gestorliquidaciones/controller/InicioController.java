6f5d9d00bc556718d6c37caeaf8b17969668e821
grupal2 <duranferreira.cristian@gmail.com>
2023-07-12 23:50:09 -0400

    creacion static html -footer, navbar1 y navbar 2, para agregar incluide en templates (no se encuentran funcionales)-

--- src/main/java/cl/cristiandurf/gestorliquidaciones/controller/InicioController.java
+++ src/main/java/cl/cristiandurf/gestorliquidaciones/controller/InicioController.java
@@ -2,10 +2,10 @@ package cl.cristiandurf.gestorliquidaciones.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 
 /**
  * @author El Piero tiene sueño
@@ -16,8 +16,9 @@ import org.apache.commons.logging.LogFactory;
 @RequestMapping("/")
 public class InicioController {
     Log logger = LogFactory.getLog(InicioController.class);
+
     @GetMapping
-    public String inicio(){
+    public String inicio() {
 
         logger.info("Estás en el home");
         return "Home";
