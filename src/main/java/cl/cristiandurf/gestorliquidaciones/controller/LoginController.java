24b367fe08625c1cbf3b612b578a784d2e65a74e
grupal2 <duranferreira.cristian@gmail.com>
2023-07-17 00:25:17 -0400

    se modifica el placeholder de RUN -EJ: 99999999 a Ingrese su rut, sin puntos ni guión-, se agrega anotaciones a LoginController

--- src/main/java/cl/cristiandurf/gestorliquidaciones/controller/LoginController.java
+++ src/main/java/cl/cristiandurf/gestorliquidaciones/controller/LoginController.java
@@ -2,12 +2,16 @@ package cl.cristiandurf.gestorliquidaciones.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/login")
 public class LoginController {
     Log logger = LogFactory.getLog(HomeController.class);
 
-    @GetMapping("/login")
+    @GetMapping
     public String inicioSesion() {
         logger.info("Estás en el login");
         return "login";
